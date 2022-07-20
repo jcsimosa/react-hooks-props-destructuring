23335868f6d507aa2fef009082dea9f22f7938ac
Jose Simosa Nunez <josecarlossimosa@gmail.com>
2022-07-20 14:13:48 -0500

    commit tree 58ed2c7e1fed83a6526c5c56d0154b983b347168

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function MovieCard({
-  title,
+function MovieCard({ 
+  title = "Sorry about this",
   posterSrc = "https://m.media-amazon.com/images/M/MV5BOTJjNzczMTUtNzc5MC00ODk0LWEwYjgtNzdiOTEyZmQxNzhmXkEyXkFqcGdeQXVyNzMzMjU5NDY@._V1_UY268_CR1,0,182,268_AL_.jpg",
   genres,
 }) {
